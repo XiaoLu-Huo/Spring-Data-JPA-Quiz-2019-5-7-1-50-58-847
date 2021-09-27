4c6f38e8845e1a1a6ca3dc016f1b1dbabc3c78f3
huoxiaolu <1821100432@qq.com>
2021-09-27 23:31:13 +0800

    [huoxiaolu] feat:完成entity以及repository接口

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,25 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private String companyName;
+
+    @NotNull
+    private Integer employeesNumber;
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,35 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Integer age;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    private Integer companyId;
+
+    @NotNull
+    private Integer salary;
 
 }
